16dd7230b6d07aabd568c0b24ad6034a2ba22cb1
Gibran Rubinger <gibranrubinger@gmail.com>
2020-05-16 14:37:02 +0100

    up
    up

--- Farm-Managment/src/main/java/ie/cct/Farm/Managment/FarmController.java
+++ Farm-Managment/src/main/java/ie/cct/Farm/Managment/FarmController.java
@@ -98,7 +98,6 @@ public class FarmController {
 			animals.add(new Cow(animal.getType(), animal.getWeight(), animal.getId()));
 			cows.add(new Cow(animal.getType(), animal.getWeight(), animal.getId()));
 			animalPost = cow;
-
 			return new SuccessResponse("This " + animalPost + " was successfully added to the system.");
 		} else if (animal.getType().equalsIgnoreCase(pig)) {
 			animals.add(new Pig(animal.getType(), animal.getWeight(), animal.getId()));
